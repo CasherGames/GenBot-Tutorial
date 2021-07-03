47d83b7737029ee7aabfc1696e0fc6eb0d445473
CasherGames <86890809+CasherGames@users.noreply.github.com>
2021-07-03 19:21:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjIwNDA2MzY1MTQ3NjI3NTIz.XXaygw.klPIAqXszSjSVP3vJ6Qwe6riB9U';
+const token = 'ODYwOTQ3OTM2NzkxNDk0NjU2.YOCqPw.Q1XW92PrB80XVMbgZWNpYv4pgOE';
 usedCommandRecently4 = new Set();
 
 client.on('ready', () =>{
